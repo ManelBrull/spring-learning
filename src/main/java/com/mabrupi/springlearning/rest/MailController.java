0c56d264d21db7e7555abb5a426a220de6172e55
ManelBrull <mbrull88@gmail.com>
2015-12-08 22:13:01 +0100

    mock mail sender now is injected in MailController

--- src/main/java/com/mabrupi/springlearning/rest/MailController.java
+++ src/main/java/com/mabrupi/springlearning/rest/MailController.java
@@ -1,15 +1,17 @@
 package com.mabrupi.springlearning.rest;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mabrupi.springlearning.mail.MailSender;
-import com.mabrupi.springlearning.mail.MockMailSender;
 
 @RestController
 public class MailController {
 
-    private MailSender mailSender = new MockMailSender();
+    @Resource
+    private MailSender mailSender;
 
     @RequestMapping("/sendMail")
     public String sendMail() {
