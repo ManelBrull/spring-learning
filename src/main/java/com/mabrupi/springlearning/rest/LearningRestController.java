2cc3250ae90ecd51fdce6beb214d76ccebf76d5d
ManelBrull <mbrull88@gmail.com>
2015-12-13 15:46:54 +0100

    added application properties to spring app

--- src/main/java/com/mabrupi/springlearning/rest/LearningRestController.java
+++ src/main/java/com/mabrupi/springlearning/rest/LearningRestController.java
@@ -1,13 +1,17 @@
 package com.mabrupi.springlearning.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LearningRestController {
 
+    @Value("${app.name}")
+    private String appName;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Spring world";
+        return "Hello Spring world from: " + appName;
     }
 }
